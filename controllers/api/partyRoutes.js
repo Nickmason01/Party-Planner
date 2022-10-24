84f5e3e0e6999aa5505cacd527295f1663d31817
Katherine fang <katherinefang314@gmail.com>
2022-10-24 19:56:39 -0400

    put

--- controllers/api/partyRoutes.js
+++ controllers/api/partyRoutes.js
@@ -1,1 +1,14 @@
+router.put("/:id", async (req, res) => {
+    const newParty = await Party.update(req.body, {
+        where: {
+            id: req.params.id,
+        },
+    });
 
+    if (!newParty) {
+        res.status(404).json({ message: "Please create a new party." });
+        return;
+    }
+    console.log(newParty);
+    res.status(200).json(newParty);
+});
