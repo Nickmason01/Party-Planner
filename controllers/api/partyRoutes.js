99a9fea1fb093b6002c0bfbe94d94633f5bb9e8f
Katherine fang <katherinefang314@gmail.com>
2022-10-24 21:30:44 -0400

    new put

--- controllers/api/partyRoutes.js
+++ controllers/api/partyRoutes.js
@@ -1,3 +1,5 @@
+const Party = require("../../models/Party");
+
 router.put("/:id", async (req, res) => {
     const newParty = await Party.update(req.body, {
         where: {
@@ -11,4 +13,19 @@ router.put("/:id", async (req, res) => {
     }
     console.log(newParty);
     res.status(200).json(newParty);
+});
+
+router.put("/:name", async (req, res) => {
+    const joinParty = await Party.update(req.body, {
+        where: {
+            name: req.params.name,
+        },
+    });
+
+    if (!joinParty) {
+        res.status(404).jsaon({ message: "Please enter your name." });
+        return;
+    }
+    console.log(joinParty);
+    res.status(200).json(joinParty);
 });
