13d0d69eec93ec74c53ebe3412b464dc94a38826
Dimitra Anastasooulos <dimitra.anasta524@gmail.com>
2022-10-24 21:41:06 -0400

    editss

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const exhdbars = require('express-handlebars');
 // will need routes and helper require here
 const routes = require('');
 const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3301;
 
-const hdbars = exhdbars.create({ helpers});
+const hdbars = exhdbars.create({ helpers });
 // custom helpers--- not sure yet if needed.
 
 const sess = {
@@ -17,7 +17,7 @@ const sess = {
     cookie: {},
     resave: false,
     saveUninitialized: true,
-    store: new SequelizeStore ({
+    store: new SequelizeStore({
         db: sequelize
     })
 };
@@ -27,7 +27,7 @@ app.use(session(sess));
 // template engines for handlebars?? I think we need them but need to review
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true})); //maybe
+app.use(express.urlencoded({ extended: true })); //maybe
 app.use(express.static(path.join(_direname, 'public'))); // not sure 
 
 app.use(routes); // this will be for the routes
